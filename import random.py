fbd5262bb4621c616db5fbc530fa84d43d95467c
parsaCr7M Dev <parsacr9@gmail.com>
2024-03-27 00:04:41 +0330

    Update import random.py
    XD to a changed

--- import random.py
+++ import random.py
@@ -1,27 +1,88 @@
-import random
-
-# Define the possible choices
-choices = ['sang', 'kagaz', 'gychi']
-
-# Let the computer make a choice
-computer_choice = random.choice(choices)
-
-# Get the user's choice
-user_choice = input("Enter a choice (sang, kagaz, gychi): ").lower()
-
-# Check if the user's choice is valid
-if user_choice not in choices:
-    print("Invalid choice. Please try again.")
+import random 
+a=['sang', 'kagaz', 'ghychi']
+computer=random.choice (a)
+man=input("Enter a choice (sang, kagaz, ghychi): ")
+if computer == "sang" and man =="ghychi":
+    print("bkhti")
+elif computer == "ghychi" and man =="sang":
+    print("bordi")
+elif computer == "kagaz" and man == "ghychi":
+    print("bordi")
+elif computer == "ghychi" and man == "kagaz":
+    print("bkhti")
+elif computer == "sang" and man == "kagaz":
+    print("bordi")
+elif computer == "kagaz" and  man == "sang":
+    print("bkhti")
 else:
-        # Determine the winner
-    if computer_choice == user_choice:
-        print("It's a tie!")
-    elif (user_choice == 'sang' and computer_choice == 'gychi') or \
-         (user_choice == 'gychi' and computer_choice == 'kagaz') or \
-         (user_choice == 'kagaz' and computer_choice == 'sang'):
-        print("You win!")
-    else:
-        print("You lose!")
+    print("mosavi")
+print(f"man {computer} entkhab karde bodam")
+print(f"\nYou chose {man}, computer chose {computer}.\n")
 
-    print(f"Computer chose {computer_choice}.")
-    print(f"You chose {user_choice}.")
+
+# user_action = input("Enter a choice (rock, paper, scissors): ")
+# possible_actions = ["rock", "paper", "scissors"]
+# computer_action = random.choice(possible_actions)
+# print(f"\nYou chose {user_action}, computer chose {computer_action}.\n")
+#
+# if user_action == computer_action:
+#     print(f"Both players selected {user_action}. It's a tie!")
+# elif user_action == "rock":
+#     if computer_action == "scissors":
+#         print("Rock smashes scissors! You win!")
+#     else:
+#         print("Paper covers rock! You lose.")
+# elif user_action == "paper":
+#     if computer_action == "rock":
+#         print("Paper covers rock! You win!")
+#     else:
+#         print("Scissors cuts paper! You lose.")
+# elif user_action == "scissors":
+#     if computer_action == "paper":
+#         print("Scissors cuts paper! You win!")
+#     else:
+#         print("Rock smashes scissors! You lose.")
+import random
+a=['sang', 'kagaz', 'ghychi']
+computer=random.choice (a)
+man=input("Enter a choice (sang, kagaz, ghychi): ")
+if computer == "sang" and man =="ghychi":
+    print("bkhti")
+elif computer == "ghychi" and man =="sang":
+    print("bordi")
+elif computer == "kagaz" and man == "ghychi":
+    print("bordi")
+elif computer == "ghychi" and man == "kagaz":
+    print("bkhti")
+elif computer == "sang" and man == "kagaz":
+    print("bordi")
+elif computer == "kagaz" and  man == "sang":
+    print("bkhti")
+else:
+    print("mosavi")
+print(f"man {computer} entkhab karde bodam")
+print(f"\nYou chose {man}, computer chose {computer}.\n")
+
+
+# user_action = input("Enter a choice (rock, paper, scissors): ")
+# possible_actions = ["rock", "paper", "scissors"]
+# computer_action = random.choice(possible_actions)
+# print(f"\nYou chose {user_action}, computer chose {computer_action}.\n")
+#
+# if user_action == computer_action:
+#     print(f"Both players selected {user_action}. It's a tie!")
+# elif user_action == "rock":
+#     if computer_action == "scissors":
+#         print("Rock smashes scissors! You win!")
+#     else:
+#         print("Paper covers rock! You lose.")
+# elif user_action == "paper":
+#     if computer_action == "rock":
+#         print("Paper covers rock! You win!")
+#     else:
+#         print("Scissors cuts paper! You lose.")
+# elif user_action == "scissors":
+#     if computer_action == "paper":
+#         print("Scissors cuts paper! You win!")
+#     else:
+#         print("Rock smashes scissors! You lose.")
